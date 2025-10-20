a9d3d913c09aa45668562bec45ce27f16f5518ce
NandoJunioDev <Frnndmaz_cf@outlook.com>
2025-10-20 13:29:24 -0300

    first commit

---
+++ src/main/java/fernando/apipasseiolancha/ApiPasseioLanchaApplication.java
@@ -0,0 +1,13 @@
+package fernando.apipasseiolancha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPasseioLanchaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPasseioLanchaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fernando/apipasseiolancha/ApiPasseioLanchaApplicationTests.java
@@ -0,0 +1,13 @@
+package fernando.apipasseiolancha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPasseioLanchaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
